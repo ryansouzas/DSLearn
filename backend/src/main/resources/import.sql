822bb19d155c39a22eb327aaaac7d7ad066763d5
Ryan Souza <ryanssebadelhe@gmail.com>
2026-06-15 19:48:12 -0300

    Course

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -14,3 +14,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu4kJKLE19b3Veca_E80yM8SBFdVkdaUkSgw&s', 'https://upload.wikimedia.org/wikipedia/commons/e/e7/Logo_Course_Croisi%C3%A8re_EDHEC_noir.png');
+
